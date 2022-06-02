ef3e8c26ec91b4cc490e26f53d2007d119e725ce
Manage0 <61554465+Manage0@users.noreply.github.com>
2022-06-02 10:11:32 +0200

    optimized

--- index.js
+++ index.js
@@ -1,13 +1,9 @@
 //https://codingshiksha.com/javascript/node-js-express-ffmpeg-mp3-pitch-and-tempo-changer-web-app-deployed-to-heroku-full-project-2020/
 
 const express = require('express')
-
-const multer = require('multer')
 const download = require('download');
 var admin = require("firebase-admin");
 const { v4: uuidv4 } = require('uuid');
-
-// eleve a storageból kéne delete, meg valami security is, stb..
 
 const partone= "cdb1840a555309aa3f6"
 const parttwo="f7b3bd01be5c922684e0e"
@@ -32,7 +28,6 @@ admin.initializeApp({
 });
 
 const storageRef = admin.storage().bucket(`gs://buisnesprotect.appspot.com`);
-const path = require('path')
 
 async function uploadFile(path, filename) {
 
@@ -44,18 +39,12 @@ async function uploadFile(path, filename) {
           firebaseStorageDownloadTokens: uuidv4(),
       }
   });
-
-
   return storage[0].metadata.mediaLink;
 }
-
-
 
 const fs = require('fs')
 
 var pitch
-
-var tempo
 
 const bodyParser = require('body-parser')
 
@@ -73,85 +62,56 @@ var subDirectory = "public/uploads";
 
 if (!fs.existsSync(dir)) {
   fs.mkdirSync(dir);
-
   fs.mkdirSync(subDirectory);
 }
 
-var storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, "public/uploads");
-  },
-  filename: function (req, file, cb) {
-    cb(
-      null,
-      file.fieldname + "-" + Date.now() + path.extname(file.originalname)
-    );
-  },
-});
-
-const audioFilter = function (req, file, cb) {
-  // Accept videos only
-  if (!file.originalname.match(/\.(mp3)$/)) {
-    req.fileValidationError = "Only audio files are allowed!";
-    return cb(new Error("Only audio files are allowed!"), false);
-  }
-  cb(null, true);
-};
-
-const upload = multer({ storage: storage, fileFilter: audioFilter })
-
 app.use(express.static("public"));
-
 
 const PORT = process.env.PORT || 3000
 
 app.use(function (req, res, next) {
-
   // Website you wish to allow to connect
   res.setHeader('Access-Control-Allow-Origin', '*');
-
   // Request methods you wish to allow
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
-
+  res.setHeader('Access-Control-Allow-Methods', 'POST');
   // Request headers you wish to allow
   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type,Access-Control-Allow-Origin');
-
   next()
 });
 
 app.post('/changepitch', async (req, res) => {
-  console.log("something landed here")
+  console.log("Endpoint triggered")
   console.log(req.body)
 
   // File URL
   const url = req.body.url;
 
   // Download the file
-  await download(url, './public');
-  //  corsHandler(req, res, () => {
+  try {
+    await download(url, './public');
+  } catch (error) {
+    console.error("Error while downloading the file from: "+url)
+    console.error(error)
+  }
   if (req.body) {
-    //heroku pw is: asdqasda1.
     pitch = 0.75
-    console.log(pitch)
-    console.log(tempo)
     const location = "public/" + req.body.riportID + "audio"
     exec(`ffmpeg -i ${location} -af asetrate=44100*${pitch},aresample=44100 ${outputFilePath}`,
-      async (err, stdout, stderr) => {
+      async (err) => {
         if (err) throw err
         res.setHeader('Access-Control-Allow-Origin', '*');
-        //set up manually
-        console.log("going to send")
-        const downloadURL = await uploadFile("./"+outputFilePath,req.body.riportID)
-        console.log(downloadURL)
-        fs.unlinkSync(location)
-        fs.unlinkSync(outputFilePath)
-        res.json({downloadURL:downloadURL})
-        console.log("we are done")
-        
+        try {
+          const downloadURL = await uploadFile("./"+outputFilePath,req.body.riportID)
+          fs.unlinkSync(location)
+          fs.unlinkSync(outputFilePath)
+          res.json({downloadURL:downloadURL})
+        } catch (error) {
+          console.error("Error while uploading the file to storage")
+          console.error(error)
+        }
       })
   }
 });
-//})
 
 app.listen(PORT, () => {
   console.log(`App is listening on Port ${PORT}`)
